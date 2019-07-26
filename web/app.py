2c305dd5f99059a1de002448b93d97f261eafa05
jiangshan123 <jiangshan321@yahoo.com>
2019-07-26 11:02:24 -0400

    add kmeans page and selection bar

--- web/app.py
+++ web/app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/test")
+@app.route("/kmeans")
 def test():
-    return "This is a test page"
+    return render_template('kmeans.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
